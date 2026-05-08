30908508c45d345c5a9cd8b960324c54dcd0b0db
Maycon Metzner <42306033+mmetznerm@users.noreply.github.com>
2026-05-08 19:12:14 -0300

    build(core): initial setup with koin, ktor and navigation

---
+++ composeApp/src/commonMain/kotlin/com/mmetzner/vehiclemaintenance/core/di/networkModule.kt
@@ -0,0 +1,20 @@
+package com.mmetzner.vehiclemaintenance.core.di
+
+import com.mmetzner.vehiclemaintenance.core.network.createHttpClient
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+val networkModule = module {
+    single { createHttpClient() }
+}
+
+fun appModules(): List<Module> = listOf(networkModule)
+
+fun initKoin(appDeclaration: KoinAppDeclaration = {}) = startKoin {
+    appDeclaration()
+    modules(appModules())
+}
+
+fun initKoinIOS() = initKoin {}
